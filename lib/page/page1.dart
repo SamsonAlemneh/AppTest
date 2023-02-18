1726075cd2de98878dd0db273ca8d9d6aa761cac
samsonalemneh <samsontil@yahoo.com>
2023-02-17 21:20:39 -0500

    Fifth Commit

---
+++ lib/page/page1.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text('Glow Effect'),
+      centerTitle: true,
+      backgroundColor: Colors.blueAccent[100],
+    ),
+  );
+
+}
