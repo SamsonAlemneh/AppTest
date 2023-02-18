86a378c949c2013f52ddd6e76d962203b1c3f7f5
samsonalemneh <samsontil@yahoo.com>
2023-02-17 22:27:42 -0500

    Ninth Commit

--- lib/page/page1.dart
+++ lib/page/page1.dart
@@ -13,6 +13,14 @@ class Page1 extends StatelessWidget {
       centerTitle: true,
       backgroundColor: Colors.blueAccent[100],
     ),
+    body:  const Center(child: Text('This is Glow Effect',
+    style: TextStyle(
+    color: Colors.amberAccent,
+    fontSize: 40.0,
+    fontWeight: FontWeight.bold,
+  ),
+    ),
+    ),
   );
 
 }
